a4ef3a9540f309d8f4be7caffb70e65ff376eab7
josh182014 <josh182014@gmail.com>
2019-11-19 16:35:20 -0700

    started player class

--- player.py
+++ player.py
@@ -1,3 +1,9 @@
 class Player:
-    def __init__(self, name):
+
+    def __init__(self, name, currentRoom, items=[]):
         self.name = name
+        self.currentRoom = currentRoom
+        self.items = items
+
+    def add_item(self, item):
+        return self.items.append(item)
