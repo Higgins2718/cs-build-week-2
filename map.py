3824b96cf2a87cee6e656b88201399202197339a
hectorsvill <hectorstevenvillasano@gmail.com>
2019-11-19 15:05:49 -0800

    clean up map

--- map.py
+++ map.py
@@ -4,7 +4,7 @@ class Path:
         self.player = player
         # create adjacency list with room as key and exits array as value
         self.mapped = dict()
-        self.saved_map = dict()
+        self.saved_map = []
 
     def dfs(self):
         path = []
@@ -15,6 +15,8 @@ class Path:
         # graph consisting of 500 rooms
         while len(self.mapped) < 499:
             if self.player.currentRoom.id not in self.mapped:
+                self.saved_map.append[self.player.currentRoom]
+                
                 currentroomID = self.player.currentRoom.room_id
                 exits = self.player.currentRoom.exits
                 # not a mapped room add to map
@@ -41,62 +43,12 @@ class Path:
                 backTrack.append("w")
             elif move == "w":
                 backTrack.append("e")
+            
+            
             # travel to next room
-
             # perfrom wait operations
             self.player.travel(move)
         # print(self.mapped)
         print(path)
-        return path
-
-
-class Path:
-    def __init__(self, player):
-        self.player = player
-        # create adjacency list with room as key and exits array as value
-        self.mapped = dict()
-        self.saved_map = dict()
-
-    def dfs(self):
-        path = []
-        backTrack = []
-
-        # {0: ['n', 's', 'w', 'e']}
-        self.mapped[self.player.currentRoom.id] = self.player.currentRoom.getExits()
-        # graph consisting of 500 rooms
-        while len(self.mapped) < 499:
-            if self.player.currentRoom.id not in self.mapped:
-                currentroomID = self.player.currentRoom.id
-                exits = self.player.currentRoom.getExits()
-                # not a mapped room add to map
-                self.mapped[currentroomID] = exits
-                self.mapped[currentroomID].remove(backTrack[-1])
-            # when players hits dead end, backtrack and store path
-            while not len(self.mapped[self.player.currentRoom.id]):
-                back = backTrack.pop()
-                path.append(back)
-
-                # perfrom wait operations
-                self.player.travel(back)
-
-            # Get next move
-            move = self.mapped[self.player.currentRoom.id].pop(0)
-            path.append(move)
-
-            # get inverse room for backTrack
-            if move == "n":
-                backTrack.append("s")
-            elif move == "s":
-                backTrack.append("n")
-            elif move == "e":
-                backTrack.append("w")
-            elif move == "w":
-                backTrack.append("e")
-            # travel to next room
-
-            # perfrom wait operations
-            self.player.travel(move)
-
-        # print(self.mapped)
-        print(path)
+        print(self.saved_map)
         return path
